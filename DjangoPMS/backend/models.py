46d97ec73f2980f4b16aad63c863dbf22e5d1e75
TommyP2W <152071432+TommyP2W@users.noreply.github.com>
2024-04-14 13:15:54 +0100

    I have implemented both the Message and Request models for the project. The Message model does not function as intended, so I have deliberately not included any of the messaging pages yet.
    I have not been able to test the request model, so there may be issues.

--- DjangoPMS/backend/models.py
+++ DjangoPMS/backend/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+
+from django.views import defaults
+
 
 # Create your models here.
 
@@ -17,3 +21,36 @@ class Admin(BaseUser):
 
 class Driver(BaseUser):
     pass
+
+class Slot(models.Model):
+    pass
+
+class Message(models.Model):
+    Message_text = models.TextField(max_length=1000)
+    timestamp = models.DateTimeField(default=timezone.now)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sender", default=User)
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name="receiver", default=User)
+    def __str__(self):
+        return self.Message_text
+
+
+
+class Request(models.Model):
+    driver_id = models.ForeignKey(Driver, on_delete=models.CASCADE)
+    slot = models.ForeignKey(Slot, on_delete=models.CASCADE)
+    arrival = models.DateTimeField()
+    departure = models.DateTimeField()
+    class current_status(models.TextChoices):
+        PENDING = "Pending"
+        APPROVED = "Approved"
+        REJECTED = "Rejected"
+
+    status = models.CharField(
+        max_length=8,
+        choices=current_status.choices,
+        default=current_status.PENDING,
+    )
+
+
+
+
