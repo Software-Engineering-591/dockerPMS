b1dc1c27f297b89f829c68e715f0231260a33d79
TommyP2W <152071432+TommyP2W@users.noreply.github.com>
2024-04-14 18:08:59 +0100

    Implementation of the Message and Request models.

--- DjangoPMS/backend/models.py
+++ DjangoPMS/backend/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+
+from django.views import defaults
+
 
 # Create your models here.
 
@@ -17,3 +21,30 @@ class Admin(BaseUser):
 
 class Driver(BaseUser):
     pass
+
+
+class Message(models.Model):
+        Message_text = models.TextField(max_length=1000)
+        timestamp = models.DateTimeField(default=timezone.now)
+        sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sender", null=False)
+        receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name="receiver", null=False)
+        def __str__(self):
+            return self.Message_text
+
+
+
+class Request(models.Model):
+    driver_id = models.ForeignKey(Driver, on_delete=models.CASCADE, null=False)
+    slot = models.ForeignKey(Slot, on_delete=models.CASCADE, null=False)
+    arrival = models.DateTimeField()
+    departure = models.DateTimeField()
+    class current_status(models.TextChoices):
+        PENDING = "Pending"
+        APPROVED = "Approved"
+        REJECTED = "Rejected"
+
+    status = models.CharField(max_length=8, choices=current_status.choices, default=current_status.PENDING)
+
+
+
+
