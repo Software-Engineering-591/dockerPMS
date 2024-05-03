4412efc8da10f3fb32cfbcc83606aa2637531527
Nitro Sniper <44097331+NitroSniper@users.noreply.github.com>
2024-05-03 19:40:52 +0100

    Reserve, Lot, ThemeSwitcher, and Other Stuff (Massive Change) (#15)
    ## My Sole Contribution
    ### Added a Reserve Page using Leaflet to power the map
    * `template/frontend/reserve/` - This is a folder containing the html of the reserve page which includes popup, the map, and the page itself.
    *  `static/frontend/reserve.js` - JS that deals with adding icons on the map
    * `static/frontend/reserve.css` - CSS that deals with map decoration
    ### Added Parking Lot Page
    * `template/frontend/lot.html` - html for parking lot
    ### Global nav and Footer
    * renamed `template/frontend/nav2.html` -> `template/frontend/nav.html` and redesigned it
    * added `template/frontend/footer.html`
    * Incorporated it in most pages 
    ### _Light Mode is Here!_
    Chose the theme `corporate` for light. you can change it in `base.html`. This value gets stored in localStorage and can be changed if you have access to the navbar
    ## Other Contribution
    ### Revamp of Message Page and API
    
    I needed to revamp most of message's html pages just because of the use of your own colours which conflicted with the theme switcher and weird bugs happening however I tried to keep to how it was laid out before.
    > **I spent my entire Wednesday night doing this for 10 hours if there is any consolation**
    
    Other than that...
    * Created a view function that figures if the user is admin or not and point them to your views so that  Admin and Driver use the same view.
    * Added said view to `urls.py` and removed your old route
    * Added your message to the profile badge so you can access the page natively
    * Placed html files in `template/frontend/message/`
    * `static/frontend/message_load.js` - JS that scrolls to the latest msg on load and <kbd>CTRL + ENTER</kbd> sends the msg
    
    ### Contact Page Additions
    * I added an example of loading images in `static/frontend/assets/` and included my pfp
    * `static/frontend/contact_switch.js` JS that flips through the screens. IT IS ANNOYING AND YOU CAN REMOVE IT IF YOU HATE IT
    * Moved the footer so it's next to the phone. 
    * Changed your css since you edited `swap` class but that was on your old navbar which I replaced with the new one
    ### General Stuff
    Fixed some bugs like....
    * SVG didn't use `stroke="currentColor"
    * general typos
    * Fixed PEP 8 Violations.

--- DjangoPMS/backend/admin.py
+++ DjangoPMS/backend/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Admin, Driver, Message, Request
+from .models import Admin, Driver, Message, Request, ParkingLot
+from leaflet.admin import LeafletGeoAdmin
 
 # Register your models here.
 
 admin.site.register([Admin, Driver, Message, Request])
+# If model implements Geospatial data
+admin.site.register(ParkingLot, LeafletGeoAdmin)
--- DjangoPMS/backend/models.py
+++ DjangoPMS/backend/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
-
-from django.views import defaults
+from django.contrib.gis.db import models as gis_models
 
 
 # Create your models here.
@@ -25,6 +24,7 @@ class Driver(BaseUser):
     def __str__(self):
         return self.user.username
 
+
 class Payment(models.Model):
     amount = models.DecimalField(max_digits=6, decimal_places=2)
     timestamp = models.DateTimeField(auto_now=True)
@@ -34,26 +34,33 @@ class Payment(models.Model):
 
 class Slot(models.Model):
     class Status(models.TextChoices):
-        RESERVED = "R"
-        AVAILABLE = "A"
-        DISABLED = "D"
+        RESERVED = 'R'
+        AVAILABLE = 'A'
+        DISABLED = 'D'
 
-    status = models.CharField(choices=Status, max_length=1, default=Status.AVAILABLE)
+    status = models.CharField(
+        choices=Status, max_length=1, default=Status.AVAILABLE
+    )
     number = models.CharField(max_length=10)
 
     # Links driver class
-    driver = models.ForeignKey(Driver, on_delete=models.SET_NULL, null=True, default=None)
-
+    driver = models.ForeignKey(
+        Driver, on_delete=models.SET_NULL, null=True, default=None
+    )
 
 
 class Message(models.Model):
-        message_text = models.TextField(max_length=1000)
-        timestamp = models.DateTimeField(default=timezone.now)
-        sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sender")
-        receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name="receiver")
-        def __str__(self):
-            return self.message_text
+    message_text = models.TextField(max_length=1000)
+    timestamp = models.DateTimeField(default=timezone.now)
+    sender = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='sender'
+    )
+    receiver = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='receiver'
+    )
 
+    def __str__(self):
+        return self.message_text
 
 
 class Request(models.Model):
@@ -61,11 +68,20 @@ class Request(models.Model):
     slot = models.ForeignKey(Slot, on_delete=models.CASCADE)
     arrival = models.DateTimeField()
     departure = models.DateTimeField()
+
     class CurrentStatus(models.TextChoices):
-        Pending = "P"
-        Approved = "A"
-        Rejected = "R"
+        PENDING = 'P'
+        APPROVED = 'A'
+        REJECTED = 'R'
 
-    status = models.CharField(max_length=1, choices=CurrentStatus, default=CurrentStatus.Pending)
+    status = models.CharField(
+        max_length=1, choices=CurrentStatus, default=CurrentStatus.PENDING
+    )
 
 
+class ParkingLot(models.Model):
+    poly = gis_models.PolygonField(geography=True)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return str(self.name)
